f5fb5fb3c6a05a93103c02e88a5c0e04344b142a
cooldesignluke <58713241+lukeylias@users.noreply.github.com>
2022-08-01 15:05:40 +1000

    new change

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -1,37 +1,47 @@
 import * as React from 'react';
 import {Button} from '@chakra-ui/button';
-import {Divider, Flex, Stack, VStack, Spacer, Container} from '@chakra-ui/layout';
+import {Divider, Flex, Stack, VStack, HStack, Spacer} from '@chakra-ui/layout';
 import {Radio, RadioGroup} from '@chakra-ui/radio';
 import {Select} from '@chakra-ui/select';
 import {FormControl, FormLabel} from '@chakra-ui/form-control';
-import {Text, Link, Heading, Box, Tag} from '@chakra-ui/react';
+import {Text, Link, Heading, Box, Icon} from '@chakra-ui/react';
 import {useEffect, useState} from 'react';
+import {HiOutlinePencil, HiOutlinePuzzle, HiOutlineBeaker, HiOutlineChat} from 'react-icons/hi';
+import * as logo from './iOS_dashboard.jpg';
 
 //
-// values for images and copy
+// values for images, keys and copy
 //
 const MAP = {
     default: {
         ios: {
-            image: 'https://iili.io/84Dmv9.png',
+            image: './iOS_dashboard.jpg',
             key: '1ae99ff96abb3fde3ca3cd9585c804361c88df68',
         },
         android: {
             image: 'https://iili.io/84DbY7.png',
             key: '45e63e28d53c29c87addc3b7e6c1dc56e88f6bd0',
         },
-        // web: 'https://iili.io/84b7aI.png',
+        web: {
+            image: 'https://iili.io/84b7aI.png',
+            key: '7942c0879b87578e74e44cce306af1f306e170ac',
+        },
         copy: 'The dashboard shows all the customers accounts and is the home of the app.',
     },
 
     emptyState: {
         ios: {
-            image: 'https://iili.io/84Dmv9.png',
+            image: 'https://iili.io/hcawFf.png',
             key: '1ae99ff96abb3fde3ca3cd9585c804361c88df68',
         },
-        //     ios: 'https://iili.io/hcawFf.png',
-        //     android: 'https://iili.io/8SC2ON.png',
-        //     web: 'https://iili.io/S7SR87.png',
+        android: {
+            image: 'https://iili.io/8SC2ON.png',
+            key: '45e63e28d53c29c87addc3b7e6c1dc56e88f6bd0',
+        },
+        web: {
+            image: 'https://iili.io/S7SR87.png',
+            key: '',
+        },
         copy: 'Log in is the area where our customers enter in their username and password to access internet banking.',
     },
 };
@@ -43,27 +53,30 @@ const App = ({}) => {
     const [pattern, setPattern] = useState('default');
     const [platform, setPlatform] = useState('ios');
     const [image, setImage] = useState('https://iili.io/84DbY7.png');
-    const [link] = useState('https://www.youtube.com/watch?v=Zo_k6l6mwdo');
+    // const [link, setLink] = useState('https://www.youtube.com/watch?v=Zo_k6l6mwdo');
 
+    // Checks for changes to pattern and platform and changes image
     useEffect(() => {
         const newImage = MAP[pattern][platform].image;
         setImage(newImage);
         console.log('updated from hook', pattern, platform);
     }, [pattern, platform]);
 
+    // Handles the image style
     const imageStyle = {
         height: '716px',
         width: 'auto',
         maxWidth: 'unset',
     };
 
+    // Sends the selected component key to the backend via this msg
     const onCreate = () => {
         parent.postMessage({pluginMessage: {componentId: MAP[pattern][platform].key}}, '*');
         console.log('updated from hook', pattern, platform);
     };
 
     //
-    // ui elements
+    // UI frontend
     //
     return (
         <Flex h="100%">
@@ -85,7 +98,7 @@ const App = ({}) => {
 
                     <FormControl>
                         <FormLabel>Pattern</FormLabel>
-                        <Select size="sm" onChange={(e) => setPattern(e.target.value)}>
+                        <Select size="md" onChange={(e) => setPattern(e.target.value)}>
                             <option value="default">Dashboard</option>
                             <option value="emptyState">Log in</option>
                         </Select>
@@ -97,13 +110,64 @@ const App = ({}) => {
                     </Stack>
 
                     <Divider />
-                    <Box bg="#F9FBFC" borderRadius="lg" p={3} align="stretch">
-                        <Stack spacing={2}>
-                            <Link color="discord.500" href={link} isExternal>
-                                Learn how the plugin works
-                            </Link>
+
+                    <Heading size="sm">Resources</Heading>
+                    <Box borderRadius="lg" p={1} align="stretch">
+                        <Stack spacing={6}>
+                            <HStack>
+                                <Icon as={HiOutlinePuzzle} color="discord.500" boxSize={6} />
+                                {/* <img src={logo} /> */}
+                                <Link
+                                    fontSize="lg"
+                                    color="discord.500"
+                                    href={
+                                        'https://www.figma.com/files/963531083469998670/project/40740182/Components?fuid=946533349950777341'
+                                    }
+                                    isExternal
+                                >
+                                    Components
+                                </Link>
+                            </HStack>
+
+                            <HStack>
+                                <Icon as={HiOutlinePencil} color="discord.500" boxSize={6} />
+                                <Link
+                                    fontSize="lg"
+                                    color="discord.500"
+                                    href={'https://greater.atlassian.net/wiki/spaces/DIG/pages/83231196/Content'}
+                                    isExternal
+                                >
+                                    Content guidelines
+                                </Link>
+                            </HStack>
+
+                            <HStack>
+                                <Icon as={HiOutlineBeaker} color="discord.500" boxSize={6} />
+                                <Link
+                                    fontSize="lg"
+                                    color="discord.500"
+                                    href={'https://greater.atlassian.net/wiki/spaces/DIG/pages/14188617/Research'}
+                                    isExternal
+                                >
+                                    Research lab
+                                </Link>
+                            </HStack>
+
+                            <HStack>
+                                <Icon as={HiOutlineChat} color="discord.500" boxSize={6} />
+                                <Link
+                                    fontSize="lg"
+                                    color="discord.500"
+                                    href={'https://greaterbankworkspace.slack.com/archives/DNWA3398U'}
+                                    isExternal
+                                >
+                                    Plugin help
+                                </Link>
+                            </HStack>
                         </Stack>
                     </Box>
+
+                    <Divider />
 
                     <Spacer />
 
@@ -119,7 +183,7 @@ const App = ({}) => {
                 p={6}
                 overflowX={'scroll'}
                 alignItems={'center'}
-                justifyContent={pattern === 'emptyState' ? 'unset' : 'center'}
+                // justifyContent={pattern === 'emptyState' ? 'unset' : 'center'}
             >
                 <img style={imageStyle} src={image} />
             </Flex>
--- src/plugin/controller.ts
+++ src/plugin/controller.ts
@@ -1,18 +1,30 @@
 
+// This is the UI panel that is shown
 figma.showUI(__html__, { height: 800, width: 1400 });
 
+// This is the message that is sent from the frontend (App.tsx)
 figma.ui.onmessage = async (msg:{componentId: string}) => {
 
+    // Selects current page user is on
     const page = figma.currentPage
     
+    // Imports the component via a key (componentId) from the frontend
     const component = (await figma.importComponentByKeyAsync(msg.componentId)).createInstance()
 
+    // Inserts that component onto the canvas
     page.insertChild(0,component)
 
-    figma.viewport.scrollAndZoomIntoView([component]);
+    // The viewport scrolls and zooms the new component into view
+    // figma.viewport.scrollAndZoomIntoView([component]);
+    const bounds = figma.viewport.bounds;
 
+    component.x = bounds.x + bounds.width / 2 - component.width / 2;
+    component.y = bounds.y + bounds.height / 2 - component.height / 2;
+
+    // Detaches instance to make it just a frame / frames
     component.detachInstance()
 
+    // Shows toast down the bottom of the screen
     figma.notify("Screens created 🤘")
     
 };
